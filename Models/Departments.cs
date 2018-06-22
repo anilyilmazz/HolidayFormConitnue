50d667e35587a97e43a73bb982d1fc00c17566ba
Anıl Yılmaz <incelefon@gmail.com>
2018-06-22 10:43:50 +0300

    Department Add

---
+++ Models/Departments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IzinFormu.Models
+{
+    public class Departments
+    {
+        public int Id { get; set; }
+        public string DepartmentName { get; set; }
+        public ApplicationUser user { get; set; }
+    }
+}
---
+++ Models/DepartmentsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IzinFormu.Models
+{
+    public class DepartmentsViewModel
+    {
+        [Required]
+        [Display(Name = "Departman Adı")]
+        public string DDepartmentName { get; set; }
+
+        [Display(Name ="Departman Yöneticisi")]
+        public string User { get; set; }
+
+        [Display(Name = "Departman Id")]
+        public int Id { get; set; }
+    }
+}
